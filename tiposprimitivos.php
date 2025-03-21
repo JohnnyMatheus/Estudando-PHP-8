3d66e4ed71316b6124d2a68af44a309219bc8aab
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-21 17:37:51 -0300

    Create tiposprimitivos.php

---
+++ tiposprimitivos.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    String:         Representa caracteres no geral (texto) ex. Johnn
+    Integer:        Representa números inteiros, EX: 10,20,30,1,5
+    Float (Double): Representa números decimais, Ex: X: 10.1, 1.5, 0.25
+    Boolean:        Representa Verdadeiro ou Falso: True or False
+*/
+
+//Exemplo String 
+$nome = 'Johnny';
+
+//Exemplo de Integer
+$idade = 30;
+
+//Exemplo Float 
+$altura = 1.85;
+
+//Exemplo de Boolean
+$programador = true;
+
+
+?>
