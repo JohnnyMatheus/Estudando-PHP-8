a8379ef917fecb31f81a0af053a3ffd0817ae92d
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-21 17:05:37 -0300

    Estudando php

---
+++ concatenacaodestrings.php
@@ -0,0 +1,14 @@
+<?php
+/*
+    Concatenar == juntar dois dados
+ */
+
+$nome = 'Johnny';
+$sobrenome = 'Matheus';
+
+echo "Hello World! $nome $sobrenome";
+echo '<br/>';
+echo "Hello World!".$nome.' '.$sobrenome;
+
+
+?>
---
+++ conceitodevariaveis.php
@@ -0,0 +1,19 @@
+<?php
+/*
+    -Toda variavel édefinida começando com o $
+    -As variaveis podem conter apenas
+        --letras
+        --numeros
+        --underline _ (sublinhado)
+        --As variaveis não podem começar com um número
+        --As variaveis diferenciam letras maiusculas de letras minusculas.
+*/
+
+$primeiroNome = 'Johnny';
+$sobrenome = 'Matheus';
+echo $primeiroNome, $sobrenome;
+echo '<br/>';
+echo $primeiroNome, $sobrenome;
+echo '<br/>';
+echo 'Hello World! ', $primeiroNome;
+?>
