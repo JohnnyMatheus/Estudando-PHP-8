cdca7f019fce25f8a2eaf4fb365b037e87aa3b30
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-22 00:49:20 -0300

    Create trabalhandoComOperadores.php

---
+++ trabalhandoComOperadores.php
@@ -0,0 +1,36 @@
+<?php
+$X= 10;
+$Y = 2;
+
+//ADIÇÃO
+echo 'ADIÇÃO';
+echo '<br/>';
+echo "$X + $Y =",$X + $Y;
+echo '<br/>';
+
+//Subtração
+echo 'SUBTRAÇÃO';
+echo '<br/>';
+echo "$X - $Y =",$X - $Y;
+echo '<br/>';
+
+//DIVISÃO
+echo 'DIVISÃO';
+echo '<br/>';
+echo "$X / $Y =",$X / $Y;;
+echo '<br/>';
+
+//MULTIPLICAÇÃO
+echo 'MULTIPLICACAO';
+echo '<br/>';
+echo "$X * $Y =",$X * $Y;;
+echo '<br/>';
+
+//EXPONENCIAÇÃO
+echo 'EXPONENCIAÇÃO';
+echo '<br/>';
+echo "$X ** $Y =",$X ** $Y;
+echo '<br/>';
+
+
+?>
