3e060a75d99f601e5b3e9608c6b72f421312cbfa
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-21 22:11:12 -0300

    Create domineStringsParte1.php

---
+++ domineStringsParte1.php
@@ -0,0 +1,25 @@
+<?php
+//Exemplo String 
+$nome = 'Johnny'; 
+
+//Concatenar
+echo $nome.$nome;
+echo '<br/>';
+echo $nome." ".$nome;
+echo '<br/>';
+
+//Exibir junto com String
+echo "Meu nome é: $nome";
+echo '<br/>';
+
+//Contar 
+echo strlen($nome);
+echo '<br/>';
+
+//EXEMPLO
+$caracteres = strlen($nome);
+echo "Seu nome é $nome e ele tem $caracteres caracteres";
+
+
+
+?>
