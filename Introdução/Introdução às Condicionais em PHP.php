0e39f5603ca32bdbc3710bab41fab8e648f68332
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-23 15:01:57 -0300

    Organizando arquivos aula

---
+++ Introdução/Introdução às Condicionais em PHP.php
@@ -0,0 +1,23 @@
+<?php
+
+//Estruturas condicionais
+
+$idade = 18;
+
+if ($idade >= 18) {
+    echo 'Maior de idade';
+} else {
+    echo 'Petista';
+}
+
+
+/*
+Operadores de comparação
+== (igual)
+!= (Diferente)
+> (Maior)
+< (Menor)
+>= (Maior ou igual a)
+<= (Menor ou igual a)
+ 
+*/
