7c5faa282b7e33557cd602a35e3237ea8e13cf74
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-23 15:20:07 -0300

    Update Lógica CondicionalParte1.php

--- Introdução/Lógica CondicionalParte1.php
+++ Introdução/Lógica CondicionalParte1.php
@@ -1,3 +1,13 @@
 <?php
+//operadores lógicos
+//Operador Lógico (E) => AND -:> NO PHP -> &&
+//VERIFICA DE AMBAS CONDIÇÕES SÃP VERDADEIRAS
 
+$idade = 18;
+$temcareira =true;
+if($idade >= 18 && $temcareira){
+echo 'Pode dirigir';
+}else{
+    echo 'Não pode dirigir';
+}
 ?>
