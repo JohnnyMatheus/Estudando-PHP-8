7576a86c405213dbd194780aa8ae8809d6549f7d
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-23 00:06:44 -0300

    Update Desafios Práticos com Arrays em PHP.php

--- Desafios Práticos com Arrays em PHP.php
+++ Desafios Práticos com Arrays em PHP.php
@@ -1,10 +1,12 @@
 <?php
 echo '<h1>Exercício 01:</h1>';
-echo '<h3>1. Crie um array chamado $cidades contendo nome de 5 cidades que você quer visitar.</h3>';
+echo '<h3>1. Crie um array chamado $Cidades contendo nome de 5 cidades que você quer visitar.</h3>';
+echo '$Cidade = ["São Miguel do Oeste", "São Paulo","Campo Grande","Cascavel","Foz do Iguaçu"]';
 
+$Cidade = ['São Miguel do Oeste', 'São Paulo','Campo Grande','Cascavel','Foz do Iguaçu'];
 
 echo '<h3>2. Imprima na tela o valor da terceira cidade deste array.</h3>';
-
+echo $Cidade[2];
 
 echo '<br />';
 echo '<br />';
