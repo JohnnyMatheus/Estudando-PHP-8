52d7696d96d102f1ea32ea259caa8337edaabd39
Johnny Matheus <128015032+JohnnyMatheus@users.noreply.github.com>
2025-03-24 01:07:10 -0300

    Create misturando php com html.php

---
+++ Introdução ao módulo 2 - Funções e estruturas condicionais/misturando php com html.php
@@ -0,0 +1,26 @@
+<?php
+    $nome2 = 'Matheus';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AULA 1</title>
+</head>
+
+<body>
+    <h1>Olá mundo</h1>
+    <h2>Meu nome é
+        <?php
+        $nome = 'Johnny';
+        echo $nome;
+        echo '<br>';
+        echo $nome2;
+        ?>
+    </h2>
+</body>
+
+</html>
