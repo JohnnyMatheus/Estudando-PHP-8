04bfbaefe5516aab2fae468fbf510f5588fc01bf
Johnny Matheus <alunotioficial@gmail.com>
2025-03-31 15:27:36 -0300

    Update Documentando com PHPDoc.php

--- Funções/Documentando com PHPDoc.php
+++ Funções/Documentando com PHPDoc.php
@@ -11,4 +11,20 @@ function somar(int $n1,int $n2):int{
     return $n1+$n2;
 }
 
+
+/**
+*Gera uma string de saudação
+*@param string $nome Nome da pessoa
+*@param string $sobrenome sobrenome da pessoa
+*@param string $idade idade da pessoa
+*return string saudacao
+**/
+function parametrosNomeados(String $nome, String $sobrenome, int $idade):String {
+
+    return "Olá, $nome $sobrenome. Você tem  anos $idade";
+
+}
+
+echo parametrosNomeados(nome: 'Johnny',sobrenome: 'Matheus', idade:30);
+echo '<br>';
 ?>
