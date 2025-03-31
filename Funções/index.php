80b0c46ecee29365ee63381f7c911d71a2bccd74
Johnny Matheus <alunotioficial@gmail.com>
2025-03-30 21:00:44 -0300

    Update index.php

--- Funções/index.php
+++ Funções/index.php
@@ -15,7 +15,8 @@ var_dump($x);
 
 echo '<br>';
 
-function verificaIdade($anoNacimento){
+function verificaIdade(int $anoNacimento):int
+{
     $anoAtual = date('Y');
     $idade = $anoAtual - $anoNacimento;
     return $idade;
@@ -43,5 +44,16 @@ echo "1994 sem acompanhante:". portaoDeBalada(1994) ."<br>";
 echo "1999 com acompanhante:". portaoDeBalada(1999,true) ."<br>";
 echo "2014 com acompanhante:". portaoDeBalada(2014,true) ."<br>";
 echo "2015 com acompanhante:". portaoDeBalada(2015,false) ."<br>";
+
+echo '<br>';
+echo '<br>';
+
+function somar(float $num1,float $num2, float $num3):float
+{
+    return  $num1 + $num2 + $num3;
+}
+
+var_dump(somar(1,1,1));
+
 
 ?>
