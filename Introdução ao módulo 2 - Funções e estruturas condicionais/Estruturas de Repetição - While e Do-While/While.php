3532a1118149b4b41d402a904904672480550a97
Johnny Matheus <alunotioficial@gmail.com>
2025-04-04 13:33:03 -0300

    Create While.php

---
+++ Introdução ao módulo 2 - Funções e estruturas condicionais/Estruturas de Repetição - While e Do-While/While.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$idade = 1;
+
+while($idade <= 10){
+    echo 'idade ='.$idade."<br/>";
+    $idade++;
+
+}
+echo "<br/>";
+
+$idade2 = 1;
+do{
+    echo 'idade ='.$idade2."<br/>";
+}while($idade2<1);
+
+echo "<br/>";
+
+$idade3 = 1;
+do{
+    echo 'idade ='.$idade3."<br/>";
+    $idade3++;
+}while($idade3<10);
+
+?>
